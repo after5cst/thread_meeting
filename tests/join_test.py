a08aee09d648653b1a5f25c76332928bd35acc7c
Joe Marley <joe.marley@live.com>
2020-01-12 17:30:55 -0700

    Add unit tests.  Ignore .vscode files.

--- tests/join_test.py
+++ tests/join_test.py
@@ -6,11 +6,29 @@ import thread_meeting
 class JoinTest(unittest.TestCase):
     
     def test_can_create_join(self):
-        join = thread_meeting.Join()
+        thread_meeting.Join()
     
     def test_can_create_two_joins_on_same_thread(self):
+        thread_meeting.Join()
+        thread_meeting.Join()
+        thread_meeting.Join()
+    
+    def test_joins_twice_on_same_thread(self):
         join1 = thread_meeting.Join()
-        join2 = thread_meeting.Join()
+        thread_meeting.Join()
+        with join1:
+            pass
+        with join1:
+            pass
+    
+    def test_cannot_join_while_joined(self):
+        join1 = thread_meeting.Join()
+        with join1:
+            with self.assertRaises(RuntimeError) as context:
+                with join1:
+                    pass
+            self.assertTrue("__join__ already called" 
+                in str(context.exception), str(context.exception))
     
     def test_cannot_create_join_while_joined(self):
         with thread_meeting.Join():
